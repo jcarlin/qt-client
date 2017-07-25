a25067da68fbf5529068f2c6e0c6e30c77bdf875
jcarlin <jcarlin@xtuple.com>
2017-07-25 11:22:10 -0500

    check for lock on unpostedPurchaseOrders actions

--- guiclient/unpostedPurchaseOrders.h
+++ guiclient/unpostedPurchaseOrders.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the xTuple ERP: PostBooks Edition, a free and
  * open source Enterprise Resource Planning software suite,
- * Copyright (c) 1999-2014 by OpenMFG LLC, d/b/a xTuple.
+ * Copyright (c) 1999-2017 by OpenMFG LLC, d/b/a xTuple.
  * It is licensed to you under the Common Public Attribution License
  * version 1.0, the full text of which (including xTuple-specific Exhibits)
  * is available at www.xtuple.com/CPAL.  By using this software, you agree
@@ -35,6 +35,9 @@ public slots:
     virtual void sPrintForms();
     virtual void sView();
     virtual bool setParams(ParameterList &);
+
+private:
+    AppLock _lock;
 };
 
 #endif // UNPOSTEDPURCHASEORDERS_H
